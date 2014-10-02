8b1188603f3ba23ba220a0a97cb1c45353e04bee
piastu <ScanNorOne@air-dmitrij.dlink>
2014-10-02 21:41:43 +0700

    Initial Commit

---
+++ translator_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  translator_cpp
+//
+//  Created by Дмитрий Некрасов on 02.10.14.
+//  Copyright (c) 2014 Дмитрий Некрасов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
